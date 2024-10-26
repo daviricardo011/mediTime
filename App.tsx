d250a82f7bd9b934c6788ae84d7a293909532300
Davi Ricardo Inocêncio dos Santos <davi.ricardo@csptech.com.br>
2024-10-26 02:17:29 -0300

    adiciona notificações de acordo com o lembrete

--- App.tsx
+++ App.tsx
@@ -1,16 +1,59 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/homeScreen";
 import FooterMenu from "./src/components/FooterMenu";
 import { RootStackParamList } from "./src/navigation/types";
-import { NavigatorScreenParams } from "@react-navigation/native";
+import * as Notifications from "expo-notifications";
 
 // Tipando o Tab
 const Tab = createBottomTabNavigator<RootStackParamList>();
 
 const App = () => {
+  useEffect(() => {
+    // Configurar notificações
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+
+    // Solicitar permissões para notificações
+    const requestPermissions = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      if (status !== "granted") {
+        const { status: newStatus } =
+          await Notifications.requestPermissionsAsync();
+        if (newStatus === "granted") {
+          console.log("Permissão de notificação concedida");
+        } else {
+          console.log("Permissão de notificação negada");
+        }
+      } else {
+        console.log("Permissão de notificação já concedida");
+      }
+    };
+
+    requestPermissions();
+  }, []);
+
+  // Função para agendar notificações (exemplo)
+  const scheduleNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Lembrete",
+        body: "Este é um lembrete para você!",
+        data: { someData: "goes here" },
+      },
+      trigger: { seconds: 2 }, // A notificação será disparada em 2 segundos
+    });
+  };
+
+  // Chame a função scheduleNotification em algum lugar do seu código
+
   return (
     <NavigationContainer>
       <View style={styles.container}>
--- src/hooks/useReminderStorage.tsx
+++ src/hooks/useReminderStorage.tsx
@@ -2,8 +2,36 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Reminder } from "../types";
 import { useState } from "react";
 import NotificationModal from "../components/NotificationModal";
+import * as Notifications from "expo-notifications";
 
 const STORAGE_KEY = "@my_reminder_list";
+
+const parseDate = (dateString: string): Date => {
+  const [day, month, year] = dateString.split("/").map(Number);
+  return new Date(year, month - 1, day); // month - 1 porque o mês em Date começa do zero
+};
+
+const scheduleNotification = async (notificationData: {
+  title: string;
+  body: string;
+  date: Date;
+  identifier: string;
+}) => {
+  await Notifications.cancelScheduledNotificationAsync(
+    notificationData.identifier
+  );
+
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: notificationData.title,
+      body: notificationData.body,
+      data: { someData: "" },
+    },
+    trigger: {
+      date: notificationData.date,
+    },
+  });
+};
 
 export const useReminderStorage = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -26,23 +54,35 @@ export const useReminderStorage = () => {
       const currentList: Reminder[] = storedList ? JSON.parse(storedList) : [];
       const updatedList = [...currentList, newItem];
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedList));
+
+      let notificationDate: Date;
+
+      if (newItem.type === "remedio" && newItem.time) {
+        const [hours, minutes] = newItem.time.split(":").map(Number);
+        notificationDate = new Date(
+          parseDate(newItem.date).setHours(hours, minutes)
+        );
+      } else {
+        // Para contas, agendar um dia antes
+        notificationDate = new Date(
+          parseDate(newItem.date).getTime() - 24 * 60 * 60 * 1000
+        );
+      }
+
+      await scheduleNotification({
+        title: newItem.title,
+        body:
+          newItem.type === "remedio"
+            ? "Hora de tomar seu remédio!"
+            : "Lembre de pagar sua conta!",
+        date: notificationDate,
+        identifier: newItem.id?.toString() || Date.now().toString(),
+      });
+
       showModal("Item adicionado com sucesso!", "success");
-      console.log("Item adicionado com sucesso!", "success");
     } catch (error) {
       console.error("Erro ao adicionar item à lista:", error);
       showModal("Erro ao adicionar item à lista.", "error");
-    }
-  };
-
-  const getList = async (): Promise<Reminder[]> => {
-    try {
-      const storedList = await AsyncStorage.getItem(STORAGE_KEY);
-      console.log("lista consultada com sucesso!", "success");
-      return storedList ? JSON.parse(storedList) : [];
-    } catch (error) {
-      console.error("Erro ao recuperar lista do armazenamento:", error);
-      showModal("Erro ao recuperar lista do armazenamento.", "error");
-      return [];
     }
   };
 
@@ -52,8 +92,10 @@ export const useReminderStorage = () => {
       const currentList: Reminder[] = storedList ? JSON.parse(storedList) : [];
       const updatedList = currentList.filter((item) => item.id !== id);
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedList));
+
+      await Notifications.cancelScheduledNotificationAsync(id.toString());
+
       showModal("Item removido com sucesso!", "success");
-      console.log("Item removido com sucesso!", "success");
     } catch (error) {
       console.error("Erro ao remover item da lista:", error);
       showModal("Erro ao remover item da lista.", "error");
@@ -71,11 +113,45 @@ export const useReminderStorage = () => {
         item.id === id ? { ...item, ...updatedItem } : item
       );
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedList));
+
+      let notificationDate: Date;
+
+      if (updatedItem.type === "remedio" && updatedItem.time) {
+        const [hours, minutes] = updatedItem.time.split(":").map(Number);
+        notificationDate = new Date(
+          parseDate(updatedItem.date).setHours(hours, minutes)
+        );
+      } else {
+        notificationDate = new Date(
+          parseDate(updatedItem.date).getTime() - 24 * 60 * 60 * 1000
+        );
+      }
+
+      await scheduleNotification({
+        title: updatedItem.title,
+        body:
+          updatedItem.type === "remedio"
+            ? "Hora de tomar seu remédio!"
+            : "Lembre de pagar sua conta!",
+        date: notificationDate,
+        identifier: id.toString(),
+      });
+
       showModal("Item atualizado com sucesso!", "success");
-      console.log("Item atualizado com sucesso!", "success");
     } catch (error) {
       console.error("Erro ao atualizar item da lista:", error);
       showModal("Erro ao atualizar item da lista.", "error");
+    }
+  };
+
+  const getList = async (): Promise<Reminder[]> => {
+    try {
+      const storedList = await AsyncStorage.getItem(STORAGE_KEY);
+      return storedList ? JSON.parse(storedList) : [];
+    } catch (error) {
+      console.error("Erro ao recuperar lista do armazenamento:", error);
+      showModal("Erro ao recuperar lista do armazenamento.", "error");
+      return [];
     }
   };
 
